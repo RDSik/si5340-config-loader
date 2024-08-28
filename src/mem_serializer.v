7bbc040e89c9e64f3086285fe3b76ed41783ce11
Dmitry <dmitryryabickov@yandex.ru>
2024-08-29 01:56:37 +0300

    Upd

--- src/mem_serializer.v
+++ src/mem_serializer.v
@@ -14,18 +14,20 @@ module mem_serializer #(
     localparam PERIOD_NS  = (1/CLK_FREQ) * 1_000_000_000;
     localparam PAUSE_NS   = (300 * 1_000_000)/PERIOD_NS;  // delay 300 msec(300_000_000 ns) and 8 ns period of clock cycle
     localparam CYCLES     = MEM_WIDTH/DATA_WIDTH;         // need 3 cycles to transmit 24 bit for 8 bit data output and pause between every byte
-    localparam SLAVE_ADDR = 8'b11101000;                  // [6:0] 1110100 - addr, [7] - write
+    localparam SLAVE_ADDR = 8'b11101_00_0;                // [6:0] 1110100 - addr, [7] - write/read bit
 
-    localparam [1:0] IDLE  = 2'b00,
-                     DATA  = 2'b01,
-                     PAUSE = 2'b10,
-                     INDEX = 2'b11;
+    localparam [2:0] IDLE  = 3'b000,
+                     DATA  = 3'b001,
+                     PAUSE = 3'b010,
+                     INDEX = 3'b011,
+                     INIT  = 3'b100;
 
     reg [$clog2(CYCLES)-1:0]      cycles_cnt;
     reg [MEM_WIDTH-1:0]           tmp;
     reg [$clog2(PAUSE_NS)-1:0]    pause_cnt;
     reg [$clog2(WORD_NUMBER)-1:0] mem_index;
     reg [1:0]                     state;
+    reg                           flag;
 
     reg [MEM_WIDTH-1:0] mem [0:WORD_NUMBER-1]; // [23:8] - addr, [7:0] - data
 
@@ -37,15 +39,18 @@ module mem_serializer #(
             pause_cnt  <= 0;
             cycles_cnt <= 0;
             tmp        <= 0;
-            state      <= IDLE;
+            flag       <= 1;
+            state      <= INIT;
         end else begin
             case (state)
                 IDLE: begin
-                    state <= DATA;
-                    tmp   <= mem[mem_index];
+                    state  <= PAUSE;
+                    tmp    <= mem[mem_index];
+                    if (flag) data_o <= SLAVE_ADDR;
                 end
                 DATA: if (ack_i) begin
-                    data_o <= tmp[cycles_cnt*8 +: 8];
+                    data_o <= tmp[cycles_cnt*DATA_WIDTH +: DATA_WIDTH];
+                    flag   <= 0;
                     if (cycles_cnt == CYCLES - 1) begin
                         cycles_cnt <= 0;
                         state      <= INDEX;
@@ -61,12 +66,15 @@ module mem_serializer #(
                     pause_cnt <= pause_cnt + 1;
                     state     <= PAUSE;
                 end
-                INDEX: begin
-                    state <= IDLE;
-                    if (mem_index == WORD_NUMBER - 1) mem_index <= 0;
-                    else mem_index <= mem_index + 1;
+                INDEX: if (mem_index == WORD_NUMBER - 1) begin
+                    mem_index <= 0;
+                    flag      <= 1;
+                    state     <= IDLE;
+                end else begin
+                    mem_index <= mem_index + 1;
+                    state     <= IDLE;
                 end
-                default: state <= IDLE;
+                default: state <= INIT;
             endcase
         end
     end
--- src/si5340_config_loader.v
+++ src/si5340_config_loader.v
@@ -14,6 +14,9 @@ module si5340_config_loader #(
     inout wire scl  // SDA-line
 );
 
+    wire [DATA_WIDTH-1:0] data;
+    wire                  ack;
+
     wire scl_pad_i;    // SCL-line input
     wire scl_pad_o;    // SCL-line output (always 1'b0)
     wire scl_padoen_o; // SCL-line output enable (active low)
@@ -21,20 +24,10 @@ module si5340_config_loader #(
     wire sda_pad_o;    // SDA-line output (always 1'b0)
     wire sda_padoen_o; // SDA-line output enable (active low)
 
-    wire [DATA_WIDTH-1:0] data;
-
-    mem_serializer #(
-        .CONFIG_MEM  (CONFIG_MEM ),
-        .MEM_WIDTH   (MEM_WIDTH  ),
-        .WORD_NUMBER (WORD_NUMBER),
-        .DATA_WIDTH  (DATA_WIDTH ),
-        .CLK_FREQ    (CLK_FREQ   )
-    ) serializer_inst (
-        .clk_i   (clk_i  ),
-        .arstn_i (arstn_i),
-        .ack_i   (),
-        .data_o  (data   )
-    );
+    assign scl_pad_i = scl;
+    assign sda_pad_i = sda;
+    assign scl       = scl_padoen_o ? 1'bz : scl_pad_o;
+    assign sda       = sda_padoen_o ? 1'bz : sda_pad_o;
 
     i2c_master_byte i2c_inst (
         .clk     (clk_i       ),
@@ -49,7 +42,8 @@ module si5340_config_loader #(
         .ack_in  (),
         .din     (data        ),
         .dout    (),
-        .cmd_ack (),
+        .cmd_ack (ack         ),
+        .ack_out (),
         .scl_i   (scl_pad_i   ),
         .scl_o   (scl_pad_o   ),
         .sda_i   (sda_pad_i   ),
@@ -58,10 +52,18 @@ module si5340_config_loader #(
         .sda_oen (sda_padoen_o)
     );
 
-    assign scl_pad_i = scl;
-    assign sda_pad_i = sda;
-    assign scl       = scl_padoen_oe ? 1'bz : scl_pad_o;
-    assign sda       = sda_padoen_oe ? 1'bz : sda_pad_o;
+    mem_serializer #(
+        .CONFIG_MEM  (CONFIG_MEM ),
+        .MEM_WIDTH   (MEM_WIDTH  ),
+        .WORD_NUMBER (WORD_NUMBER),
+        .DATA_WIDTH  (DATA_WIDTH ),
+        .CLK_FREQ    (CLK_FREQ   )
+    ) serializer_inst (
+        .clk_i   (clk_i  ),
+        .arstn_i (arstn_i),
+        .ack_i   (ack    ),
+        .data_o  (data   )
+    );
 
     `ifdef COCOTB_SIM
         initial begin
