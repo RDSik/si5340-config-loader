11b2f2f5e88f1e659e4fc4867a82c4140c2a3c46
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2024-09-11 16:16:46 +0300

    Update Manifest.py

--- syn/Manifest.py
+++ syn/Manifest.py
@@ -3,7 +3,7 @@ action = "synthesis"
 
 syn_family  = "Cyclone 10 GX"
 syn_device  = "10cx220Y"
-syn_grade   = "I5G"
+syn_grade   = "E5G"
 syn_package = "F780"
 
 syn_top     = "si5340_config_loader"
