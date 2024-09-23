0841d94f6fb7dedc135b81810e8ed53b1cfd56e9
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2024-09-23 19:16:21 +0300

    Update Manifest.py

--- src/Manifest.py
+++ src/Manifest.py
@@ -3,7 +3,6 @@ files = [
     "i2c_master_byte.v",
     "i2c_master_defines.v",
     "timescale.v",
-    # "mem_serializer.v",
     "si5340_config_loader.sv",
     "i2c_ctrl_if.sv",
     "cfg_pkg.svh",
