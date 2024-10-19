fbf229191a58645bc712096234f77bf278db226e
RDSik <dmitryryabickov@yandex.ru>
2024-10-20 01:29:15 +0300

    Upd manifest

--- src/Manifest.py
+++ src/Manifest.py
@@ -1,6 +1,6 @@
 files = [
-    "i2c_master_bit.v",
-    "i2c_master_byte.v",
+    "i2c_master_bit_ctrl.v",
+    "i2c_master_byte_ctrl.v",
     "i2c_master_defines.v",
     "timescale.v",
     "si5340_config_loader.sv",
