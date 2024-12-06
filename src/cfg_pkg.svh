961520fb30412f8f0e786d49158b9895c488f328
Dmitry <dmitryryabickov@yandex.ru>
2024-12-06 23:18:30 +0300

    upd top

--- src/cfg_pkg.svh
+++ src/cfg_pkg.svh
@@ -14,7 +14,7 @@ package cfg_pkg;
     parameter CYCLES     = MEM_WIDTH/DATA_WIDTH;         // need 3 cycles to transmit 24 bit for 8 bit data output and pause between every byte
 
     parameter CLK_FREQ   = 125_000_000;                  // clock freaquency
-    parameter PERIOD_NS  = (1/CLK_FREQ) * 1_000_000_000; // period of clock cycle in ns
+    parameter PERIOD_NS  = 1_000_000_000/CLK_FREQ; // period of clock cycle in ns
     // parameter PAUSE_NS   = (300 * 1_000_000)/PERIOD_NS;  // delay 300 msec(300_000_000 ns) and 8 ns period
     
     parameter SLAVE_ADDR = 7'b111_0100;                  // [6:0] 1110100 - addr, [7] - read/write bit
